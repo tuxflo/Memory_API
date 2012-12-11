80ef1b338884285983001bbfa33ddb5507b3f925
Florian Hennig <flo.hennig@gmail.com>
2012-12-11 13:55:46 +0100

    ToDo: remove Player IDs
    the ids are not used and should be removed

--- main.cpp
+++ main.cpp
@@ -9,14 +9,13 @@ int main()
     Board board(3,4);
     vector<Player> players(2);
     players.at(0).set_name_id("tuxflo", 0);
-    players.at(1).set_name_id("anne", 0);
+    players.at(1).set_name_id("anne", 1);
     board.init_game(&players);
 
     while(1)
     {
         board.choose_card();
     }
-
     return 0;
 }
 
